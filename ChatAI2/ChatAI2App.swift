541c3bdfdf784825d1bebc7d1371562028c106ed
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-03 22:20:16 -0500

    Initial Commit

---
+++ ChatAI2/ChatAI2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAI2App.swift
+//  ChatAI2
+//
+//  Created by Nicholas Gramenos on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAI2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatAI2
+//
+//  Created by Nicholas Gramenos on 3/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
