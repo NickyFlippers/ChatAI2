ad30a995aa5a1ccfefd18632aa5f45b14c21c074
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-04 02:14:20 -0500

    My ChatGPT API app attempt

--- ChatAI2/ContentView.swift
+++ ChatAI2/ContentView.swift
@@ -5,22 +5,84 @@
 //  Created by Nicholas Gramenos on 3/3/23.
 //
 
+import OpenAISwift
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+final class ViewModel: ObservableObject {
+    init() {}
+
+    private var client: OpenAISwift?
+    func setup() {
+        client = OpenAISwift(authToken: "")
+    }
+    
+    func send(text: String, completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text,
+                               maxTokens: 500,
+                               completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+            completion(output)
+            case .failure:
+                break
+            }
+        })
     }
 }
 
+
+
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(models, id: \.self) { string in
+                Text(string)
+            }
+            
+            Spacer()
+            
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("Send") {
+                    send()
+                }
+            }
+        }
+        .onAppear {
+            viewModel.setup()
+        }
+        .padding()
+    }
+    
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        models.append("Me: \(text)")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                self.models.append("ChatGPT: "+response)
+                self.text = "" // Clear the text field
+            }
+        }
+    }
+}
+        
+        
+        
+        
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+        
+        
+  
